96340c54229cddc9611e83afada87ada73741567
Andres Rojas <andres.rojas@auto1.com>
2018-09-24 17:00:17 +0200

    Add car picture in the response

--- src/data/cars.js
+++ src/data/cars.js
@@ -11,6 +11,7 @@ for (var i = 0; i < 1000; i++) {
     brand: Random.getRandomItemFromCollection(brands),
     color: Random.getRandomItemFromCollection(colors),
     price: Random.getRandomNumber(100000),
+    picture_url: 'http://localhost:3001/car.svg'
   });
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const port = 3001;
 
 const Cars = require("./controllers/cars");
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(function(req, res, next) {
   const { originalUrl, method } = req;
   console.log(`${method} ${originalUrl}`);
