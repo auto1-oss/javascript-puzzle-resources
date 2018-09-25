c1842b21b952b6087c7334ebcb205e67578997b0
Andres Rojas <andres.rojas@auto1.com>
2018-09-25 11:03:11 +0200

    Add year to car response

--- src/data/cars.js
+++ src/data/cars.js
@@ -11,6 +11,7 @@ for (var i = 0; i < 1000; i++) {
     brand: Random.getRandomItemFromCollection(brands),
     color: Random.getRandomItemFromCollection(colors),
     price: Random.getRandomNumber(100000),
+    year: Random.getRandomNumber(60) + 1958, // 1958 to 2018
     picture_url: 'http://localhost:3001/car.svg'
   });
 }
