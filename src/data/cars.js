b6f9f277fe46e0debe510eda0073e8e914db52d4
Andres Rojas <andres.rojas@auto1.com>
2018-09-28 15:24:21 +0200

    Add more properties to the cars in the response
    - Added model, stock number, mileage and fuel type.
    - Also renamed model to manufacturer.

--- src/data/cars.js
+++ src/data/cars.js
@@ -1,17 +1,27 @@
 const Random = require("../lib/random");
 
 const colors = require("./colors");
-const brands = require("./brands");
+const manufacturers = require("./manufacturers");
 
 const cars = [];
 
 for (var i = 0; i < 1000; i++) {
+  const manufacturer = Random.getRandomItemFromCollection(manufacturers);
+
+  const model = Random.getRandomItemFromCollection(manufacturer.models);
+  const stockNumber = Random.getRandomNumber(89999) + 10000;
+  const color = Random.getRandomItemFromCollection(colors);
+  const mileage = Random.getRandomNumber(99999) + 100000;
+  const fuelType = Random.getRandomItemFromCollection(['Petrol', 'Diesel']);
+
   cars.push({
     id: i,
-    brand: Random.getRandomItemFromCollection(brands),
-    color: Random.getRandomItemFromCollection(colors),
-    price: Random.getRandomNumber(100000),
-    year: Random.getRandomNumber(60) + 1958, // 1958 to 2018
+    manufacturer: manufacturer.name,
+    model,
+    stockNumber,
+    mileage,
+    fuelType,
+    color,
     picture_url: 'http://localhost:3001/car.svg'
   });
 }
