e304e4afd9c96a364c47cbd5265f4afe4845c4ad
Andres Rojas <andres.rojas@auto1.com>
2018-10-11 15:17:17 +0200

    Setup swagger documentation UI

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const carsController = require("./controllers/cars");
 const colorsController = require("./controllers/colors");
 const manufacturersController = require("./controllers/manufacturers");
@@ -7,4 +9,5 @@ module.exports = function routes(app) {
   app.get("/cars/:stockNumber", carsController.getCar);
   app.get("/colors", colorsController.getColors);
   app.get("/manufacturers", manufacturersController.getManufacturers);
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
