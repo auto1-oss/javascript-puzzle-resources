5124f828b94ce9c044d9caf69238a0f858c8e3f6
Andres Rojas <andres.rojas@auto1.com>
2018-11-23 11:09:57 +0100

    Add CORS to mock server

--- mock-server/src/index.js
+++ mock-server/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const routes = require("./routes");
 
 const app = express();
 const port = 3001;
 
 app.use(express.static(__dirname + "/public"));
+app.use(cors());
 
 routes(app);
 
